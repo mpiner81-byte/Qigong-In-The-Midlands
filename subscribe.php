9f8e88266324ccae4dc0e4afabca2f6672d9fa7a
mpiner81-byte <mpiner81@gmail.com>
2026-03-07 11:51:50 -0500

    Update subscribe.php

--- subscribe.php
+++ subscribe.php
@@ -1,3 +1,4 @@
+```php
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
@@ -9,15 +10,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Sanitize email
     $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
 
+    // Validate email
     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
 
         $to = "qigonginthemidlands@gmail.com";
         $subject = "New Subscriber - Qigong in the Midlands";
         $message = "New subscriber email: " . $email;
 
-        $headers = "From: website@qigonginthemidlands.com\r\n";
+        $headers = "From: Qigong Website <qigonginthemidlands@gmail.com>\r\n";
         $headers .= "Reply-To: " . $email . "\r\n";
+        $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
+        // Send email
         mail($to, $subject, $message, $headers);
 
         // Redirect to thank you page
@@ -29,3 +33,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
+```
